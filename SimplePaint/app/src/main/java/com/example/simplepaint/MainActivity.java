60db9b63f5dbe4e56e38dde826e0cc0a224b0bcf
RenSoares <advresoares@yahoo.com.br>
2022-03-10 20:51:04 -0300

    Input btn calcular

--- SimplePaint/app/src/main/java/com/example/simplepaint/MainActivity.java
+++ SimplePaint/app/src/main/java/com/example/simplepaint/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewCanvas viewCanvas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewCanvas = findViewById(R.id.area_desenho);
+
     }
 }
--- SimplePaint/app/src/main/java/com/example/simplepaint/ViewCanvas.java
+++ SimplePaint/app/src/main/java/com/example/simplepaint/ViewCanvas.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -18,12 +19,18 @@ public class ViewCanvas extends View {
     private Linha linha;
 
     public ViewCanvas(Context context, @Nullable AttributeSet attrs){
-        super(context, attrs);
+       super(context, attrs);
     }
 
     private void inicializaObjetos(){
         path = new Path();
         linha = new Linha(getContext(), path);
+    }
+
+    private void inicioToque(float x, float y){
+        path.moveTo(x,y);
+        eixoX = x;
+        eixoY = y;
     }
 
     private void moveDedoTela(float x, float y){
@@ -47,4 +54,21 @@ public class ViewCanvas extends View {
         invalidate();
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        linha.desenharLinha(canvas);
     }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas (bitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
