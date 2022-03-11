e263e6e5d694f376e8cce579034f6e1a69dcc3b2
RenSoares <advresoares@yahoo.com.br>
2022-03-11 13:01:54 -0300

    Calculo da Quantidade de água a beber.

--- LembrarBeberAgua/app/src/main/java/com/rensoares/lembrarbeberagua/MainActivity.kt
+++ LembrarBeberAgua/app/src/main/java/com/rensoares/lembrarbeberagua/MainActivity.kt
@@ -1,14 +1,75 @@
 package com.rensoares.lembrarbeberagua
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import com.rensoares.lembrarbeberagua.model.CalcularIngestaoDiaria
+import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edit_peso: EditText
+    private lateinit var edit_idade:EditText
+    private lateinit var bt_calcular: Button
+    private lateinit var txt_resultado_ml: TextView
+    private lateinit var ic_redefinir_dados: ImageView
+
+    private lateinit var calcularIngestaoDiaria: CalcularIngestaoDiaria
+    private var resultadoMl = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+        IniciarComponentes()
+        calcularIngestaoDiaria = CalcularIngestaoDiaria()
+
+        bt_calcular.setOnClickListener {
+
+            if (edit_peso.text.toString().isEmpty()){
+                Toast.makeText(this, R.string.toast_informe_peso, Toast.LENGTH_SHORT).show()
+            }else if (edit_idade.text.toString().isEmpty()){
+                Toast.makeText(this, R.string.toast_informe_idade, Toast.LENGTH_SHORT).show()
+            }ele{
+                val peso = edit_peso.text.toString().toDouble()
+                val idade = edit_idade.text.toString().toInt()
+                calcularIngestaoDiaria.CalcularTotalML(peso, idade)
+                resultadoMl = calcularIngestaoDiaria.ResultadoMl()
+                val formatar = NumberFormat.getNumberInstance(Locale("pt", "BR"))
+                formatar.isGroupingUsed = false
+                txt_resultado_ml.text = formatar.format(resultadoMl) + " " + "ml"
+            }
+        }
+
+        ic_redefinir_dados.setOnClickListener {
+
+            val alertDialog = AlertDialog(this)
+            alertDialog.setTitle(R.string.dialog_titulo)
+                .setMessage(R.string.dialog_desc)
+                .setPositiveButton("OK", {dialogInterface, i ->
+                    edit_peso.setText("")
+                    edit_idade.setText("")
+                    txt_resultado_ml.text = ""
+                })
+            alertDialog.setNegativeButton("Cancelar", {dialogInterface, i ->})
+            val dialog = alertDialog.create()
+            dialog.show()
+        }
+    }
+    private fun IniciarComponentes(){
+        edit_peso = findViewById(R.id.edit_peso)
+        edit_idade = findViewById(R.id.edit_idade)
+        bt_calcular = findViewById(R.id.bt_calcular)
+        txt_resultado_ml = findViewById(R.id.txt_resultado_ml)
+        ic_redefinir_dados = findViewById(R.id.ic_redefinir)
+
 
     }
+}
+
+private fun Any.setMessage(dialogDesc: Int): Any {
+
 }
